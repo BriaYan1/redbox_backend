81df0f65866e29e06e52f566306f0fb83888f889
BriaYan1 <briayang1997@gmail.com>
2026-04-19 20:16:30 -0400

    Migraciones listas

--- RedBoxAppBackend/backend/admin.py
+++ RedBoxAppBackend/backend/admin.py
@@ -1,3 +1,36 @@
 from django.contrib import admin
+from backend.models import Patologias
+from backend.models import Roles
+from backend.models import Usuarios
+from backend.models import Usuario_roles
+from backend.models import Biometrias
+from backend.models import Entrenadores
+from backend.models import Planes
+from backend.models import Usuario_planes   
+from backend.models import Accesos
+from backend.models import Pagos
+from backend.models import Clases
+from backend.models import Planificacion_diaria
+from backend.models import Movimientos
+from backend.models import Resultados
+from backend.models import Reservas
+
 
 # Register your models here.
+
+admin.site.register(Patologias)
+admin.site.register(Roles)
+admin.site.register(Usuarios)
+admin.site.register(Usuario_roles)
+admin.site.register(Biometrias)
+admin.site.register(Entrenadores)
+admin.site.register(Planes)
+admin.site.register(Usuario_planes)
+admin.site.register(Accesos)
+admin.site.register(Pagos)
+admin.site.register (Clases)
+admin.site.register (Planificacion_diaria)
+admin.site.register (Movimientos)
+admin.site.register (Resultados)
+admin.site.register (Reservas)
+
--- RedBoxAppBackend/backend/models.py
+++ RedBoxAppBackend/backend/models.py
@@ -1,9 +1,190 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+class Roles(models.Model):
+    # Definimos las constantes para los roles
+    ADMINISTRADOR = 'Administrador'
+    USUARIO = 'Usuario'
+    ENTRENADOR = 'Entrenador'
+
+    # Creamos la lista de opciones (tupla de tuplas)
+    OPCIONES_ROLES = [
+        (ADMINISTRADOR, 'Administrador'),
+        (USUARIO, 'Usuario'),
+        (ENTRENADOR, 'Entrenador'),
+    ]
+
+    id_rol = models.AutoField(primary_key=True)
+    nombre_rol = models.CharField(
+        max_length=50,
+        choices=OPCIONES_ROLES, 
+        default=USUARIO,        
+        unique=True             
+    )
+
+    def __str__(self):
+        return self.nombre_rol
+
+
+class Usuarios(models.Model):
+    MASCULINO = 'M'
+    FEMENINO = 'F'
+    OTRO = 'Otro'
+
+    # Creamos la lista de opciones (tupla de tuplas)
+    OPCIONES_GENERO = [
+        (MASCULINO, 'M'),
+        (FEMENINO , 'F'),
+        (OTRO, 'Otro'),
+
+    ]
+    
+    id_usuario = models.AutoField(primary_key=True)
+    id_auth_user = models.IntegerField(unique=True)
+    nombre_usuario = models.CharField(max_length=100)
+    apellido_usuario = models.CharField(max_length=100)
+    email_usuario = models.EmailField(unique=True)
+    cedula_usuario = models.CharField(max_length=20, unique=True)
+    telefono_usuario = models.CharField(max_length=20, unique=True)
+    fecha_nacimiento_usuario = models.DateField()
+    genero_usuario = models.CharField(
+        max_length= 5,
+        choices=OPCIONES_GENERO,      
+        unique=True             
+    )
+    fecha_creacion_usuario = models.DateTimeField(auto_now_add=True)
+    activo_usuario = models.BooleanField(default=True)
+
+class Usuario_roles(models.Model):
+    id_usuario_rol = models.AutoField(primary_key=True)
+    id_usuario = models.ForeignKey(Usuarios, on_delete=models.CASCADE)
+    id_rol = models.ForeignKey(Roles, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.nombre_usuario
+    
+class Biometria(models.Model):
+    id_biometria = models.AutoField(primary_key=True)
+    id_usuario = models.ForeignKey(Usuarios, on_delete=models.CASCADE)
+    tipo_biometria = models.CharField(max_length=100)
+    huella_hash = models.CharField(max_length=100)
+    fecha_registro_biometria = models.DateTimeField(auto_now_add=True)
+    activo_biometria = models.BooleanField(default=True)
+    
+class Planes(models.Model):
+    id_plan = models.AutoField(primary_key=True)
+    nombre_plan = models.CharField(max_length=100)
+    cantidad_clases = models.IntegerField()
+    precio_plan = models.DecimalField(max_digits=10, decimal_places=2)
+    descripcion_plan = models.TextField()
+
+class Usuario_planes(models.Model):
+    id_usuario_plan = models.AutoField(primary_key=True)
+    id_usuario = models.ForeignKey(Usuarios, on_delete=models.CASCADE)
+    id_plan = models.ForeignKey(Planes, on_delete=models.CASCADE)
+    fecha_inicio_plan = models.DateField()
+    fecha_fin_plan = models.DateField()
+    clases_restantes = models.IntegerField()
+    activo_usuario_plan = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.nombre_usuario
+    
+
+class Accesos(models.Model):
+    id_acceso = models.AutoField(primary_key=True)
+    id_usuario = models.ForeignKey(Usuarios, on_delete=models.CASCADE)
+    fecha_acceso = models.DateTimeField(auto_now_add=True)
+    tipo_acceso = models.CharField(max_length=100)
+    permitido_acceso = models.BooleanField(default=True)
+    motivo_acceso = models.TextField()
+    
+class Pagos(models.Model):
+    
+    COMPLETADO = 'Completado'
+    PENDIENTE = 'Pendiente'
+    FALLIDO = 'Fallido'
+
+    ESTADOS_PAGO= [
+        (COMPLETADO, 'Completado'),
+        (PENDIENTE, 'Pendiente'),
+        (FALLIDO, 'Fallido'),
+    ]
+    
+    id_pago = models.AutoField(primary_key=True)
+    id_usuario_plan = models.ForeignKey(Planes, on_delete=models.CASCADE)
+    monto = models.DecimalField(max_digits=10, decimal_places=2)
+    moneda = models.CharField(max_length=10)
+    fecha_pago = models.DateTimeField(auto_now_add=True)
+    metodo_pago = models.CharField(max_length=100)
+    comprobante_pago = models.CharField(max_length=100)
+    estado_pago = models.CharField(max_length=100) 
+    
+    estado_pago = models.CharField(
+        max_length=20,
+        choices=ESTADOS_PAGO,
+        default=PENDIENTE, # Por defecto, todo pago entra como pendiente
+    )
+
 class Patologias(models.Model):
     id_patologia = models.AutoField(primary_key=True)
     nombre_patologia = models.CharField(max_length=100)
 
     def __str__(self):
         return self.title
+    
+class Usuario_patologias(models.Model):
+    id_usuario_patologia = models.AutoField(primary_key=True)
+    id_usuario = models.ForeignKey(Usuarios, on_delete=models.CASCADE)
+    id_patologia = models.ForeignKey(Patologias, on_delete=models.CASCADE)
+    observaciones_patologia = models.TextField()
+    
+class Clases(models.Model):
+    id_clase = models.AutoField(primary_key=True)
+    id_usuario = models.ForeignKey(Usuarios, on_delete=models.CASCADE)
+    fecha_clase = models.DateTimeField()
+    hora_inicio_clase = models.TimeField()
+    hora_fin_clase = models.TimeField()
+    cupo_maximo_clase = models.IntegerField()
+    descripcion_clase = models.TextField()
+    
+class Planificacion_diaria(models.Model):
+    id_planificacion = models.AutoField(primary_key=True)
+    id_clase = models.ForeignKey(Clases, on_delete=models.CASCADE)
+    entrenamiento = models.TextField()
+    observaciones_planificacion_diaria = models.TextField()
+
+class Movimientos(models.Model):
+    id_movimiento = models.AutoField(primary_key=True)
+    nombre_movimiento = models.CharField(max_length=100)
+
+class Resultados(models.Model):
+    id_resultado = models.AutoField(primary_key=True)
+    id_usuario = models.ForeignKey(Usuarios, on_delete=models.CASCADE)
+    id_movimiento = models.ForeignKey(Movimientos, on_delete=models.CASCADE)
+    fecha_evaluacion = models.DateTimeField(auto_now_add=True)
+    fecha_registro_resultado = models.DateTimeField(auto_now_add=True)
+    peso = models.DecimalField(max_digits=10, decimal_places=2)
+    repeticiones = models.IntegerField()
+    rondas = models.IntegerField()
+    comentarios_resultado = models.TextField()
+    
+class Reservas(models.Model):
+    ASISTIDO = 'Asistido'
+    CANCELADO = 'Cancelado'
+    
+    ESTADOS_RESERVAS = [
+        (ASISTIDO, 'Asistido'),
+        (CANCELADO, 'Cancelado'),
+    ]
+    
+    id_reserva = models.AutoField(primary_key=True)
+    id_usuario = models.ForeignKey(Usuarios, on_delete=models.CASCADE)
+    id_clase = models.ForeignKey(Clases, on_delete=models.CASCADE)
+    fecha_reserva = models.DateTimeField(auto_now_add=True)
+    estado_reservas = models.CharField(
+        max_length=20,
+        choices=ESTADOS_RESERVAS,
+    )
